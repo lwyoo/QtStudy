d207a50f1ec1987b5eaf27465c6028f98273d254
이용우(IVIS) <lywoo@ivisolution.com>
2020-01-16 11:19:08 +0900

    [RCC] qrc 파일을 rcc 파일로 변경후 사용

--- TestQRC/main.cpp
+++ TestQRC/main.cpp
@@ -1,20 +1,24 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QResource>
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    /*
+     * 생성한 rcc 파일을 등록 ()
+     * qrc 파일을 rcc파일로 변경
+     * rcc -binary qml.qrc -o qml.rcc
+     * 응용 프로그램에서 리소스 등록
+     * QResource :: registerResource( "/home/ldg/QtExample/study/QtStudy/TestQRC/qml.rcc" );
+      */
+
+    QResource::registerResource(qApp->applicationDirPath() + "/resource.rcc");
+
+    //이진 바이너리로된 rcc파일에서 qrc의 파일 읽기
     const QUrl url(QStringLiteral("qrc:/MyQRC/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-        &app, [url](QObject* obj, const QUrl& objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
